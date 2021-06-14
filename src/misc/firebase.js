223708d9ab0b63d95460cea077f0f202cdd9e370
Avdhut <avdhutshinde86000.com>
2021-06-15 02:33:31 +0530

    initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyCxpmWGPpibcwjbM2x2N9k4Zy9OUJsjIAE',
+  authDomain: 'chat-web-app-d3b70.firebaseapp.com',
+  projectId: 'chat-web-app-d3b70',
+  storageBucket: 'chat-web-app-d3b70.appspot.com',
+  messagingSenderId: '1033057028497',
+  appId: '1:1033057028497:web:1ad9546ca57ea42029b230',
+};
+
+const app = firebase.initializeApp(config);
