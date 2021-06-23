07fd344be4063dc4032ccdb5d75fc87888e25839
Avdhut <avdhutshinde86000.com>
2021-06-24 00:42:59 +0530

    created layout for chat page

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { Loader } from 'rsuite';
+import { useParams } from 'react-router';
+import { useRooms } from '../../context/rooms.context';
+
+import ChatTop from '../../components/chat-window/top';
+import Messages from '../../components/chat-window/messages';
+import ChatBottom from '../../components/chat-window/bottom';
 
 const Chat = () => {
-  return <div>hiiiii</div>;
+  const { chatId } = useParams();
+
+  const rooms = useRooms();
+
+  if (!rooms) {
+    return <Loader center vertical size="md" content="Loding" speed="slow" />;
+  }
+  const currentRoom = rooms.find(room => room.id === chatId);
+
+  if (!currentRoom) {
+    return <h6 className="text-center mt-page">Chat{chatId} not found</h6>;
+  }
+  return (
+    <>
+      <div className="chat-top">
+        <ChatTop />
+      </div>
+      <div className="chat-middle">
+        <Messages />
+      </div>
+      <div className="chat-bottom">
+        <ChatBottom />
+      </div>
+    </>
+  );
 };
 
 export default Chat;
