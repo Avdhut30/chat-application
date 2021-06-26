80a2b23b218eb3059a9452603107810d90748337
Avdhut <avdhutshinde86000.com>
2021-06-27 03:02:13 +0530

    handled messages with like

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -2,17 +2,18 @@ import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import { useCurrentRoom } from '../../../context/current-room.context';
-import { useHover } from '../../../misc/custom-hooks';
+import { useHover, useMediaQuery } from '../../../misc/custom-hooks';
 import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 import IconBtnControl from './IconBtnControl';
 
-const MessageItem = ({ message, handleAdmin }) => {
-  const { author, createdAt, text } = message;
+const MessageItem = ({ message, handleAdmin, handleLike }) => {
+  const { author, createdAt, text, likes, likeCount } = message;
 
-  const [selfRef, isHovered] = useHover();
+  const [selfHover, isHovered] = useHover();
+  const isMobile = useMediaQuery('(max-width:992px)');
 
   const isAdmin = useCurrentRoom(v => v.isAdmin);
   const admins = useCurrentRoom(v => v.admins);
@@ -21,10 +22,14 @@ const MessageItem = ({ message, handleAdmin }) => {
   const isAuthor = auth.currentUser.uid === author.uid;
   const canGrantAdmin = isAdmin && !isAuthor;
 
+  const canShowIcons = isMobile || isHovered;
+
+  const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
+
   return (
     <li
       className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-02' : ''}`}
-      ref={selfRef}
+      ref={selfHover}
     >
       <div className="d-flex align-items-center font-bolder mb-1">
         <PresenceDot uid={author.uid} />
@@ -35,7 +40,7 @@ const MessageItem = ({ message, handleAdmin }) => {
           className="ml-1"
           size="xs"
         />
-        <span className="ml-2">{author.name}</span>
+
         <ProfileInfoBtnModal
           profile={author}
           appearance="link"
@@ -49,18 +54,22 @@ const MessageItem = ({ message, handleAdmin }) => {
             </Button>
           )}
         </ProfileInfoBtnModal>
+
         <TimeAgo
           datetime={createdAt}
-          className="font-normal text-black-45 ml-2"
+          className="font-normal  text-black-45  ml-2"
         />
+
         <IconBtnControl
-          {...(true ? { color: 'red' } : {})}
-          isVisible
+          {...(isLiked ? { color: 'red' } : {})}
+          isVisible={canShowIcons}
           iconName="heart"
           tooltip="Like this message"
-          badgeContent={5}
+          onClick={() => handleLike(message.id)}
+          badgeContent={likeCount}
         />
       </div>
+
       <div>
         <span className="word-breal-all">{text}</span>
       </div>
